e36a9efaeb1cf7a9bbe551fa1cb145b00507303f
latishasumardy <sumardy5031@mydusd.org>
2019-03-16 18:22:05 -0700

    add problem description

--- RepeatedElements.java
+++ RepeatedElements.java
@@ -1,9 +1,11 @@
 
 /**
- * Write a description of class RepeatedElements here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * In a array A of size 2N, there are N+1 unique elements, and exactly one of 
+ * these elements is repeated N times.
+
+Return the element repeated N times.
+
+
  */
 public class RepeatedElements
 {
